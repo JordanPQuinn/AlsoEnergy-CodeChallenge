c611479f38b3277e79d5fb144bb2514399bef063
Jordan Quinn <jpquinn605@gmail.com>
2018-02-24 10:54:45 -0700

    Challenge Part Two Completed

--- main.js
+++ main.js
@@ -10,6 +10,27 @@ const employees = [
 
 // Part 2 Answer Here
 
+const newName = ({nameOrder, first, last}) => {
+  return nameOrder === 'reverse' ? 
+    { name: last + ' ' + first } :
+    { name: first + ' ' + last }
+}
+
+const employeeReducer = (acc, employee) => {
+  if(!acc[employee.group]) {
+    acc[employee.group] = []
+  }
+  const employeeName = newName(employee)
+  acc[employee.group] = [...acc[employee.group], employeeName]
+  return acc;
+}
+
+const newEmployeeObject = (employees) => {
+  return employees.reduce(employeeReducer, {})
+}
+
+console.log(newEmployeeObject(employees))
+
 /*****************************************************************************************
 * Bonus
 ****************************************************************************************/
